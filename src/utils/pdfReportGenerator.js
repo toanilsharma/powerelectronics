99d5a7c9cc01b6e2e930429b8fcef6656cee638c
Sai <toanilsharma@gmail.com>
2026-08-20 22:15:34 +0530

    Fix blank screen: delete circular jsx/js wrapper files causing self-import loops

--- src/utils/pdfReportGenerator.js
+++
@@ -1,5 +0,0 @@
-import * as pdfGenTsx from './pdfReportGenerator';
-
-export const generateStartReportPDF = pdfGenTsx.generateStartReportPDF;
-
-export default pdfGenTsx;
--- src/utils/waveformBus.js
+++
@@ -1,7 +0,0 @@
-import * as waveformBusTsx from './waveformBus';
-
-export const exportWaveformToHarmonicsLab = waveformBusTsx.exportWaveformToHarmonicsLab;
-export const loadWaveformFromBus = waveformBusTsx.loadWaveformFromBus;
-export const subscribeWaveformBus = waveformBusTsx.subscribeWaveformBus;
-
-export default waveformBusTsx;
